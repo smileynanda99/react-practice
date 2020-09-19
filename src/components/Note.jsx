aab3fee95ed2fa1bd33f7ca1373b6991415fef8b
RK NANDA <60211001+smileynanda99@users.noreply.github.com>
2020-09-19 11:32:28 +0530

    simple keeper app

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="note">
+      <h1>Little</h1>
+      <p>This is content</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const name = "Rohit";
-var welcomeMSG = "";
-const date = new Date();
-const year = date.getFullYear();
-const time = date.toLocaleString("en-US", {
-  hour: "2-digit",
-  minute: "numeric"
-});
-//console.log(time);
-const style = {
-  color: "black",
-  fontSize: "50px"
-};
+import App from "./components/App";
 
-if ("PM" === time.slice(6, 8)) {
-  style.color = "red";
-  welcomeMSG = "Good Night";
-} else {
-  style.color = "green";
-  welcomeMSG = "Good Morning";
-}
-ReactDOM.render(
-  <div>
-    <h1 style={style}>
-      Hello {name} {welcomeMSG}!
-    </h1>
-    <p>Copyright {year}.</p>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
