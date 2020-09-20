e6062ce7b3e548d9bd4735ad341bba4b653314b7
RK NANDA <60211001+smileynanda99@users.noreply.github.com>
2020-09-20 11:29:36 +0530

    timer watch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import Header from "./Header";
-import Note from "./Note";
-import Footer from ".//Footer";
 
 function App() {
+  setInterval(updateTime, 1000);
+  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  // function add() {
+  //   setCount(count + 1);
+  // }
+
+  // function minus() {
+  //   setCount(count - 1);
+  // }
   return (
-    <div>
-      <Header />
-      <Note />
-      <Footer />
+    <div className="style">
+      <h1>{time}</h1>
+      {/* <button className="btn" onClick={minus}>
+        -
+      </button>
+      <button className="btn" onClick={add}>
+        +
+      </button> */}
     </div>
   );
 }
